33919dc9199934acf9804eb990a6c1a1e10d0f9c
Sonal <mittal.sonal04@gmail.com>
2020-05-16 11:56:32 -0400

    Signed-off-by: Sonal <mittal.sonal04@gmail.com>

---
+++ src/test/java/devdemo/Createemail.java
@@ -0,0 +1,12 @@
+package devdemo;
+
+public class Createemail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Create an email code");
+	}
+
+}
---
+++ src/test/java/devdemo/DeleteEmail.java
@@ -0,0 +1,12 @@
+package devdemo;
+
+public class DeleteEmail {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Delete an email code");
+	}
+
+}
